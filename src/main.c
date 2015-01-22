2396bccc82d6d94a52cb8b52caec1394cb7083b9
ppatrik <patrik.pekarcik@upjs.sk>
2015-01-22 14:09:45 +0100

    #1 getline implemented and fixed signed to unsigned comparsion bug

--- src/main.c
+++ src/main.c
@@ -19,6 +19,59 @@ struct ZoznamNavstev {
 	struct ZoznamNavstev *_next;
 };
 
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL)
+    	return -1;
+
+    if (stream == NULL)
+    	return -1;
+
+    if (n == NULL)
+    	return -1;
+
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF)
+    	return -1;
+
+    if (bufptr == NULL) {
+    	bufptr = malloc(128);
+    	if (bufptr == NULL)
+    		return -1;
+
+    	size = 128;
+    }
+
+    p = bufptr;
+    while(c != EOF) {
+    	if ((int)(size - 1) < (int)(p - bufptr)) {
+    		size = size + 128;
+    		bufptr = realloc(bufptr, size);
+    		if (bufptr == NULL)
+    			return -1;
+
+    	}
+    	*p++ = c;
+    	if (c == '\n') {
+    		break;
+    	}
+    	c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+
 struct Navsteva *createVisit(int day, int hour, int minute, int timeSpent, char *name)
 {
 	struct Navsteva *item = NULL;
@@ -124,7 +177,7 @@ struct ZoznamNavstev *loadFromFile(char *filename)
 
     printf("Zacinam citat zo suboru...\n");
 
-    while ((getline(&line, &len, fr)) != -1) {
+    while ((int)(getline(&line, &len, fr)) != -1) {
         visit = malloc(sizeof(struct Navsteva));
         sscanf(line, "%d %d:%d %d %s\n", &visit->day, &visit->hour, &visit->minute, &visit->timeSpent, visit->name);
         addVisit(list, visit);
