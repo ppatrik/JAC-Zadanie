3db495b4c02c9f0fef46b14213fb9c51db67f3f7
ppatrik <patrik.pekarcik@upjs.sk>
2015-01-22 23:01:10 +0100

    finalized project

--- src/main.c
+++ src/main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "zadanie2.h"
 
 #define true 1
 #define false 0
+#define MAX 100
 
 struct Navsteva {
 	int day;
 	int hour;
 	int minute;
 	int timeSpent;
-	char name[100];
+	char name[MAX];
 };
 
 struct ZoznamNavstev {
@@ -75,6 +77,19 @@ size_t getline(char **lineptr, size_t *n, FILE *stream) {
 struct Navsteva *createVisit(int day, int hour, int minute, int timeSpent, char *name)
 {
 	struct Navsteva *item = NULL;
+
+	/* kontrola vstupu */
+	if(day < 1 && day > 31)
+        return NULL;
+	if(hour < 0 && hour >= 24)
+        return NULL;
+	if(minute < 0 && minute >= 60)
+        return NULL;
+	if(timeSpent < 0 && timeSpent >= 86400)
+        return NULL;
+    if(name == NULL)
+        return NULL;
+
 	item = malloc(sizeof(struct Navsteva));
 
 	if(item == NULL) {
@@ -95,6 +110,13 @@ void addVisit(struct ZoznamNavstev *list, struct Navsteva *visit)
 {
     struct ZoznamNavstev *newVisit;
 
+    if(visit == NULL)
+        return;
+    if(list == NULL) {
+        printf("List is not initialized!\n");
+        return;
+    }
+
     if(list->current == NULL) {
         list->current = visit;
         return;
@@ -102,6 +124,8 @@ void addVisit(struct ZoznamNavstev *list, struct Navsteva *visit)
 
     newVisit = malloc(sizeof(struct ZoznamNavstev));
     newVisit->current = visit;
+    newVisit->_next = NULL;
+
     while (list->_next != NULL) {
         list = list->_next;
     }
@@ -110,6 +134,11 @@ void addVisit(struct ZoznamNavstev *list, struct Navsteva *visit)
 
 void listVisitsRow(struct Navsteva *visit)
 {
+    if(visit == NULL) {
+        printf("Visit is not inicialized\n");
+        return;
+    }
+
     printf("|  %02d | %02d:%02d | %07d | %s\n",
         visit->day,
         visit->hour,
@@ -130,6 +159,10 @@ void printLine(int lastLine)
 void listVisits(struct ZoznamNavstev *list)
 {
     int total = 0;
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return;
+    }
 
     printLine(false);
     printf("| DEN | CAS   | TRVANIE | MENO                     |\n");
@@ -151,6 +184,11 @@ void listVisits(struct ZoznamNavstev *list)
 
 void printVisit(struct Navsteva *visit)
 {
+    if(visit == NULL) {
+        printf("Visit is not inicialized\n");
+        return;
+    }
+
     printLine(false);
     printf("Deň v mesiaci: %d\n", visit->day);
     printf("Čas: %02d:%02d\n", visit->hour, visit->minute);
@@ -167,6 +205,11 @@ struct ZoznamNavstev *loadFromFile(char *filename)
     char *line = NULL;
     size_t len;
 
+    if(filename == NULL || strcmp(filename, "") == 0) {
+        printf("Wrong filename\n");
+        return NULL;
+    }
+
     list = malloc(sizeof(struct ZoznamNavstev));
     list->current = NULL;
     list->_next = NULL;
@@ -175,11 +218,16 @@ struct ZoznamNavstev *loadFromFile(char *filename)
     printf("Otvaram subor na citanie...\n");
     fr = fopen(filename, "r");
 
+    if(fr==NULL) {
+        printf("Súbor nebol nájdený");
+        return NULL;
+    }
+
     printf("Zacinam citat zo suboru...\n");
 
     while ((int)(getline(&line, &len, fr)) != -1) {
         visit = malloc(sizeof(struct Navsteva));
-        sscanf(line, "%d %d:%d %d %s\n", &visit->day, &visit->hour, &visit->minute, &visit->timeSpent, visit->name);
+        sscanf(line, "%d %d:%d %d %49[^\t.\n]%*c", &visit->day, &visit->hour, &visit->minute, &visit->timeSpent, visit->name);
         addVisit(list, visit);
     }
 
@@ -196,6 +244,15 @@ void saveToFile(struct ZoznamNavstev *list, char *filename)
 {
     FILE *fw;
     int total;
+
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return;
+    }
+    if(filename == NULL || strcmp(filename, "") == 0) {
+        printf("Wrong filename\n");
+        return;
+    }
 
     printLine(false);
     printf("Otvaram subor na zapisovanie...\n");
@@ -234,6 +291,11 @@ float getAverageSpentOfOneVisit(struct ZoznamNavstev *list)
     float sum = 0;
     int total = 0;
 
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return 0;
+    }
+
     do {
         if(list->current == NULL && list->_next==NULL)
             break;
@@ -252,6 +314,12 @@ float getAverageSpentOfOneVisit(struct ZoznamNavstev *list)
 struct Navsteva *getLongestVisit(struct ZoznamNavstev *list)
 {
     struct Navsteva *longestVisit = NULL;
+
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return NULL;
+    }
+
     do {
         if(list->current == NULL && list->_next==NULL)
             break;
@@ -262,13 +330,26 @@ struct Navsteva *getLongestVisit(struct ZoznamNavstev *list)
         }
         list = list->_next;
     } while(list != NULL);
+
     return longestVisit;
 }
 
 struct ZoznamNavstev *getListOfName(struct ZoznamNavstev *list, char *name)
 {
     struct ZoznamNavstev *nameList;
+
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return NULL;
+    }
+    if(name == NULL || strcmp(name, "") == 0) {
+        printf("Wrong name\n");
+        return NULL;
+    }
+
     nameList = malloc(sizeof(struct ZoznamNavstev));
+    nameList->current = NULL;
+    nameList->_next = NULL;
 
     do {
         if(list->current == NULL && list->_next==NULL)
@@ -287,6 +368,16 @@ struct ZoznamNavstev *getListOfName(struct ZoznamNavstev *list, char *name)
 int getTotalSpentTimeOfName(struct ZoznamNavstev *list, char *name)
 {
     int total = 0;
+
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return -1;
+    }
+    if(name == NULL || strcmp(name, "") == 0) {
+        printf("Wrong name\n");
+        return -1;
+    }
+
     list = getListOfName(list, name);
 
     do {
@@ -306,6 +397,11 @@ int getBestDay(struct ZoznamNavstev *list)
     int countByDay[31];
     int bestDay = 0;
     int i;
+
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return -1;
+    }
 
     for(i=0; i < 31; i++) {
         countByDay[i] = 0;
@@ -330,11 +426,18 @@ float getTotalIncome(struct ZoznamNavstev *list, float price)
 {
     int total = 0;
 
+    if(list == NULL) {
+        printf("List is not inicialized\n");
+        return 0;
+    }
+
     do {
         if(list->current == NULL && list->_next==NULL)
             break;
         if(list->current != NULL) {
-            total += (int) list->current->timeSpent / 60.;
+            total += (int) list->current->timeSpent / 60;
+            if(list->current->timeSpent % 60 > 0)
+                total++;
         }
         list = list->_next;
     } while(list != NULL);
@@ -345,53 +448,159 @@ float getTotalIncome(struct ZoznamNavstev *list, float price)
 int main(void)
 {
     struct ZoznamNavstev *list = NULL;
-    struct Navsteva *visit1;
-    struct Navsteva *visit2;
-    struct Navsteva *visit3;
-    if(false) {
-        /* from file*/
-        list = loadFromFile("output.txt");
-    } else {
-        list = malloc(sizeof(struct ZoznamNavstev));
-        list->current = NULL;
-        list->_next = NULL;
-        visit1 = createVisit(1, 5, 0, 400, "Jozko Mrkvička");
-        addVisit(list, visit1);
-        visit2 = createVisit(2, 7, 10, 1000, "Jozko Mrkvička");
-        addVisit(list, visit2);
-        visit3 = createVisit(4, 9, 11, 45, "Jozko Blby");
-        addVisit(list, visit3);
+    struct ZoznamNavstev *helpList = NULL;
+    struct Navsteva *visit;
+    int endRequested = false;
+    int n;
+    float price;
+
+    int inputInt;
+    char inputString[MAX] = "";
+
+    list = malloc(sizeof(struct ZoznamNavstev));
+    list->current = NULL;
+    list->_next = NULL;
+
+    while (endRequested == false) {
+        printf("- MENU ---------------------\n");
+        printf(" [1] - Načítaj zo súboru\n");
+        printf(" [2] - Ulož do súboru\n");
+        printf(" [3] - Vytvor a pridaj návštevu\n");
+        printf(" [4] - Výpis n-tej návštevy\n");
+        printf(" [5] - Výpis všetkých návštev\n");
+        printf(" [6] - Priemerný čas jednej návštevy\n");
+        printf(" [7] - Najdlhšia návšteva\n");
+        printf(" [8] - Vypíš zoznam návštev jedného návštevníka\n");
+        printf(" [9] - Celkový čas návštev jedného návštevníka\n");
+        printf("[10] - Deň s najväčším počtom návštev\n");
+        printf("[11] - Celkový zárobok\n");
+        printf(" [0] - Zatvoriť\n");
+
+        printf("Zadajte akciu: ");
+        scanf("%d", &inputInt);
+        printf("\n");
+
+        switch (inputInt) {
+            case 1:
+                printLine(false);
+                printf("Zadajte názov súboru: ");
+                scanf("%s", inputString);
+                printf("\n");
+                list = loadFromFile(inputString);
+                if(list==NULL) {
+                    endRequested = true;
+                }
+                break;
+            case 2:
+                printLine(false);
+                printf("Zadajte názov súboru: ");
+                scanf("%s", inputString);
+                printf("\n");
+                saveToFile(list, inputString);
+                break;
+            case 3 :
+                visit = malloc(sizeof(struct Navsteva));
+
+                printLine(false);
+                printf("Pridanie novej návštevy\n");
+                printf("Zadajte deň: ");
+                scanf("%d", &visit->day);
+                printf("Zadajte hodinu: ");
+                scanf("%d", &visit->hour);
+                printf("Zadajte minutu: ");
+                scanf("%d", &visit->minute);
+                printf("Zadajte celkovy cas: ");
+                scanf("%d", &visit->timeSpent);
+                printf("Zadajte meno: ");
+                scanf(" %49[^\t.\n]%*c", visit->name);
+
+                visit = createVisit(visit->day, visit->hour, visit->minute, visit->timeSpent, visit->name);
+                printVisit(visit);
+                addVisit(list, visit);
+
+
+                break;
+            case 4 :
+                printf("Zadajte poradové číslo návštevy: ");
+                scanf("%d", &inputInt);
+
+                n = 0;
+                visit = NULL;
+
+                helpList = list;
+                if(helpList==NULL) {
+                    break;
+                }
+                do {
+                    if(helpList->current == NULL && helpList->_next==NULL)
+                        break;
+
+                    if(helpList->current != NULL) {
+                        n++;
+                        if(n==inputInt) {
+                            visit = helpList->current;
+                            break;
+                        }
+                    }
+                    helpList = helpList->_next;
+                } while(helpList != NULL);
+
+                printVisit(visit);
+
+                break;
+            case 5:
+                listVisits(list);
+                break;
+            case 6:
+                printLine(false);
+                printf("Priemerná dĺžka návštevy je: %.2f \n", getAverageSpentOfOneVisit(list));
+                printLine(true);
+                break;
+            case 7:
+                printLine(false);
+                printf("Najdlhšia návšteva je:\n");
+                printVisit(getLongestVisit(list));
+                break;
+            case 8:
+                printf("Zadajte meno: ");
+                scanf(" %49[^\t.\n]%*c", inputString);
+
+                listVisits(getListOfName(list, inputString));
+                break;
+            case 9:
+                printf("Zadajte meno: ");
+                scanf(" %49[^\t.\n]%*c", inputString);
+
+                printLine(false);
+                printf("Celkovy čas klienta '%s' je: %d\n", inputString, getTotalSpentTimeOfName(list, inputString));
+                printLine(true);
+                break;
+            case 10:
+                printLine(false);
+                printf("Najlepsim dnom bol %d. den v mesiaci\n", getBestDay(list));
+                printLine(true);
+                break;
+            case 11:
+                printf("Zadajte cenu za hodinu: ");
+                scanf("%f", &price);
+
+                printLine(false);
+                printf("Celkovy zarobok pri cene %.2f €: %.2f€\n", price, getTotalIncome(list, price));
+                printLine(true);
+                break;
+            case 0:
+                printf("\nDovidenia");
+                endRequested = true;
+                break;
+            default:
+                printf("\nWrong input\n");
+        }
+        printf("\n");
+        if(endRequested==false) {
+            printf("Press any key to continue\n");
+            getchar();
+            getchar();
+        }
     }
-
-	listVisits(list);
-
-	/*printVisit(visit2);*/
-
-	/*saveToFile(list, "output.txt");*/
-
-	printLine(false);
-	printf("Priemerná dĺžka návštevy je: %.2f \n", getAverageSpentOfOneVisit(list));
-	printLine(true);
-
-	printLine(false);
-	printf("Najdlhšia návšteva je:\n");
-	printVisit(getLongestVisit(list));
-
-	printLine(false);
-	printf("Navstevy klienta 'Jozko Blby' sú:\n");
-	listVisits(getListOfName(list, "Jozko Blby"));
-
-	printLine(false);
-	printf("Celkovy čas klienta 'Jozko Mrkvička' je: %d\n", getTotalSpentTimeOfName(list, "Jozko Mrkvička"));
-	printLine(true);
-
-	printLine(false);
-	printf("Celkovy zarobok pri cene 0.50 €: %.2f€\n", getTotalIncome(list, 0.5));
-	printLine(true);
-
-	printLine(false);
-	printf("Najlepsim dnom bol %d. den v mesiaci\n", getBestDay(list));
-	printLine(true);
-
-	return 0;
+    return 0;
 }
--- src/zadanie2.h
+++ src/zadanie2.h
@@ -2,37 +2,37 @@ struct Navsteva;
 struct ZoznamNavstev;
 
 /* Načítanie dát zo súboru */
-struct ZoznamNavstev *loadFromFile(char *filename); /* done */
+struct ZoznamNavstev *loadFromFile(char *filename); /* done 1 */
 
 /* Vytváranie nových návštev */
-struct Navsteva *createVisit(int day, int hour, int minute, int timeSpent, char name[]); /* done */
+struct Navsteva *createVisit(int day, int hour, int minute, int timeSpent, char name[]); /* done 3 */
 
 /* Pridávanie návštev do zoznamu návštev */
-void addVisit(struct ZoznamNavstev *list, struct Navsteva *visit); /* done */
+void addVisit(struct ZoznamNavstev *list, struct Navsteva *visit); /* done part of 3 */
 
 /* Vypísanie návštevy na obrazovku vo vhodnom tvare */
-void printVisit(struct Navsteva *visit); /* done */
+void printVisit(struct Navsteva *visit); /* done 4 */
 
 /* Vypísanie celého zoznamu na obrazovku v prijateľnom tvare */
-void listVisits(struct ZoznamNavstev *list); /* done */
+void listVisits(struct ZoznamNavstev *list); /* done 5 */
 
 /* Uloženie všetkých návštev zo zoznamu do súboru v takom tvare, ktorý môže byť opäť našim vstupným súborom */
-void saveToFile(struct ZoznamNavstev *list, char *filename); /* done */
+void saveToFile(struct ZoznamNavstev *list, char *filename); /* done 2 */
 
 /* Vypočítanie priemernej dĺžky jednej návštevy plavárne */
-float getAverageSpentOfOneVisit(struct ZoznamNavstev *list); /* done */
+float getAverageSpentOfOneVisit(struct ZoznamNavstev *list); /* done 6 */
 
 /* Nájdenie najdlhšie trvajúcej návštevy  plavárne */
-struct Navsteva *getLongestVisit(struct ZoznamNavstev *list); /* done */
+struct Navsteva *getLongestVisit(struct ZoznamNavstev *list); /* done 7 */
 
 /* Vytvorenie nového zoznamu všetkých návštev zadaného návštevníka */
-struct ZoznamNavstev *getListOfName(struct ZoznamNavstev *list, char *name); /* done */
+struct ZoznamNavstev *getListOfName(struct ZoznamNavstev *list, char *name); /* done 8 */
 
 /* Vypočítanie celkového času, ktorý strávil zadaný návštevník na plavárni */
-int getTotalSpentTimeOfName(struct ZoznamNavstev *list, char *name); /* done */
+int getTotalSpentTimeOfName(struct ZoznamNavstev *list, char *name); /* done 9 */
 
 /* Nájdenie dňa v týždni, počas ktorého sme zaznamenali najväčší počet návštev */
-int getBestDay(struct ZoznamNavstev *list); /* done */
+int getBestDay(struct ZoznamNavstev *list); /* done 10 */
 
 /* Vypočítanie celkových príjmov plavárne, ak určíme cenu za každú začatú hodinu */
-float getTotalIncome(struct ZoznamNavstev *list, float price); /* done */
+float getTotalIncome(struct ZoznamNavstev *list, float price); /* done 11 */
